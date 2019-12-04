c79c74d2b0e21f6738201828ce7b35e8841471a2
Elodie DEHACHE <elodie.dehache@efrei.net>
2019-12-04 22:32:56 +0100

    whatever

---
+++ ChatApp1/ChatApp1/Client.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DistantChat
+{
+    class Client
+    {
+
+        public void Main()
+        {
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 8080);
+
+                StreamReader reader = new StreamReader(client.GetStream());
+                StreamWriter writer = new StreamWriter(client.GetStream());
+
+                String s = String.Empty;
+
+                while (!(s.Equals("Exit")))
+                {
+                    Console.WriteLine("enter message to send to the server");
+                    s = Console.ReadLine();
+                    Console.WriteLine();
+                    writer.WriteLine(s);
+                    writer.Flush();
+                    String server_string = reader.ReadLine();
+                    Console.WriteLine(server_string);
+                }
+                reader.Close();
+                writer.Close();
+                client.Close(); 
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+          
+          }
+
+        }
+    }
+
