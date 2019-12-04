2c341f407eaf52a006d95ff740eba9ba09a3e8de
Elodie DEHACHE <elodie.dehache@efrei.net>
2019-12-04 20:18:06 +0100

    server init

---
+++ ChatApp1/ChatApp1/Server.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace DistantChat
+{
+    /**
+     * Class representing my server 
+     */
+    class Server
+    {
+        private int port; 
+
+        public Server(int port)
+        {
+            this.port = port; 
+
+        }
+
+        public void start()
+        {
+            TcpListener tcpListener = new TcpListener(new IPAddress(new byte[] { 127, 0, 0, 1 }), port);
+            tcpListener.Start(); 
+
+        }
+    }
+}
