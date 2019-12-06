d2fde97c4921664a78d5304402c070c3479dbb7d
Elodie DEHACHE <elodie.dehache@efrei.net>
2019-12-06 15:45:54 +0100

    Message class and net class to send and receive binary messages

--- ChatApp1/ClientForm/ChatForm.Designer.cs
+++ ChatApp1/ClientForm/ChatForm.Designer.cs
@@ -31,46 +31,48 @@
             this.Online = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.sendMessage = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // Online
             // 
             this.Online.FormattingEnabled = true;
-            this.Online.Location = new System.Drawing.Point(654, 59);
+            this.Online.Location = new System.Drawing.Point(530, 23);
             this.Online.Name = "Online";
             this.Online.Size = new System.Drawing.Size(120, 342);
             this.Online.TabIndex = 0;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(52, 384);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(26, 398);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(472, 20);
+            this.textBox1.Size = new System.Drawing.Size(472, 26);
             this.textBox1.TabIndex = 1;
             // 
             // sendMessage
             // 
-            this.sendMessage.Location = new System.Drawing.Point(530, 381);
+            this.sendMessage.Location = new System.Drawing.Point(530, 391);
             this.sendMessage.Name = "sendMessage";
-            this.sendMessage.Size = new System.Drawing.Size(75, 23);
+            this.sendMessage.Size = new System.Drawing.Size(118, 37);
             this.sendMessage.TabIndex = 2;
             this.sendMessage.Text = "Send";
             this.sendMessage.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // richTextBox1
             // 
-            this.textBox2.Location = new System.Drawing.Point(52, 59);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
+            this.richTextBox1.Location = new System.Drawing.Point(26, 23);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(487, 342);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
             // 
             // ChatForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(665, 443);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.sendMessage);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.Online);
@@ -86,6 +88,6 @@
         private System.Windows.Forms.ListBox Online;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button sendMessage;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
---
+++ ChatApp1/Communication/Net.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace Communication
+{
+    public class Net
+    {
+        public static void sendMessage(Stream s, Message m)
+        {
+            BinaryFormatter binary = new BinaryFormatter();
+            binary.Serialize(s,m); 
+        }
+
+        public static Message receiveMessage(Stream s, Message m)
+        {
+            BinaryFormatter binary = new BinaryFormatter();
+            return (Message)binary.Deserialize(s);
+        }
+
+    }
+}
